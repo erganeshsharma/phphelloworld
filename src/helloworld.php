1d92ccb24824f15cea3e01e5d103d7655eea35aa
[erganeshsharma] <[ganeshsharmait@gmail.com]>
2017-08-23 14:27:01 +0530

    Added Yellow color in the list.

--- src/helloworld.php
+++ src/helloworld.php
@@ -6,6 +6,7 @@ echo "List of Colors:<br><br>";
 echo "This is <font color=\"blue\">Blue</font>!<br>";
 echo "This is <font color=\"red\">Red</font>!<br>";
 echo "This is <font color=\"green\">Green</font>!<br>";
+echo "This is <font color=\"yellow\">Yellow</font>!<br>";
 ?>
 </body>
 </html>
