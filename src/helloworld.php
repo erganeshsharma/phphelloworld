e493af598ef3fbd948668fc48283709191941c02
[erganeshsharma] <[ganeshsharmait@gmail.com]>
2017-08-23 13:56:20 +0530

    Add helloworld.php

---
+++ src/helloworld.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<?php
+echo "Hello World!<br><br>";
+echo "List of Colors:<br><br>";
+echo "This is <font color=\"blue\">Blue</font>!<br>";
+echo "This is <font color=\"red\">Red</font>!<br>";
+echo "This is <font color=\"green\">Green</font>!<br>";
+?>
+</body>
+</html>
